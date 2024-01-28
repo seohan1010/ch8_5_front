fda56c7119c0d9cbeae75329263e9216b213db85
seohan1010 <seohan1010@hotmail.com>
2024-01-28 20:22:05 +0900

    boardList is progressing

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -1,45 +1,32 @@
-import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import classes from "../style/pages_style/boardList.module.css";
 import { useEffect, useState } from "react";
-import { boardListAction } from "../store/slices/boardListReducer";
+import * as api from "../api/api";
 
 type board = { bno: string; writer: string; title: string; writeDate: string };
-
-const filteredList = (state: any) => {
-  return state.boardList.boardList === undefined
-    ? []
-    : state.boardList.boardList;
-};
 
 const BoardList = () => {
   const [selectedFiles, setSelectedFiles] = useState<any>();
 
-  const dispatch = useDispatch();
-
   useEffect(() => {
-    dispatch(boardListAction.GET_BOARDLIST_REQUESTED(""));
-    console.log("dispatched at boardPage");
+    const payload = { page: 2, pageSize: 10 };
+    const parameter = { type: "", payload: payload };
+
+    api.getBoardList(parameter);
   }, []);
-
-  const boardList = useSelector(filteredList);
-
-  const onSelectFile = (e: any) => {};
 
   return (
     <>
       <div className={classes.table_wrap}>
         this is boardList
         <table className={classes.table}>
-          <thead className={classes.thead}>
-          <tr>
-            <td>writer</td>
-            <td>title</td>
-            <td>date</td>
-          </tr>
-        </thead>
-          <tbody className={classes.tbody}>{"list"}</tbody>
-          <tfoot className={classes.tfoot}>this is t foot</tfoot>
+          <tbody>
+            <tr>
+              <td>writer</td>
+              <td>title</td>
+              <td>date</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </>
