9f5faf5ac7004a029475209e95b639d4a44d8634
seohan1010 <seohan1010@hotmail.com>
2024-02-15 22:31:42 +0900

    BoardDetail.tsx 업데이트

--- src/components/BoardDetail.tsx
+++ src/components/BoardDetail.tsx
@@ -1,4 +1,4 @@
-import { useParams,Link } from "react-router-dom";
+9import { useParams,Link } from "react-router-dom";
 import classes from "../style/pages_style/boardDetail.module.css";
 import { useEffect } from "react";
 import BoardComment  from "./BoardComment";
@@ -27,7 +27,11 @@ const BoardDetail = () => {
 
 
       <BoardComment bno={params.bno} />
+
+ <BoardComment bno={params.bno} />
       
+
+
     </div>
   )
 };
