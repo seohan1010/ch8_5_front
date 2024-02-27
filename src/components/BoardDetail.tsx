3e7a872e4ef18f8ef85154fd42f907bd07e9b578
seohan1010 <SEOhan12@>
2024-02-27 23:36:43 +0900

    there is issue on boardDetail have to fix it

--- src/components/BoardDetail.tsx
+++ src/components/BoardDetail.tsx
@@ -5,8 +5,8 @@ import BoardComment  from "./BoardComment";
 import { getBoardDetail } from "../api/api";
 
 
-type board = {
-
+interface board{
+  
   bno : number;
   content : string;
   deletedYn : string;
@@ -14,6 +14,7 @@ type board = {
   viewCnt : number;
   writeDate : string;
   writer : string;
+  
 }
 
 
@@ -22,35 +23,23 @@ const BoardDetail = () => {
   const params = useParams();
   const [board ,setBoard] = useState<board>();
 
-
-  const test = {
-    bno : '',
-    content : '',
-    deletedYn : '',
-    title : '',
-    viewCnt : '',
-    writeDate : '',
-    writer :''
-  }
-
   useEffect(() => {
+
     const bno : any = params.bno;
+    
     const boardDetail = async ()=>{
 
       const data = await getBoardDetail(bno);
       console.log(data);
       setBoard(data);
-      test.bno=data.bno;
-      test.content=data.content;
-      test.title = data.title;
-      test.writer= data.writer;
       
     }
-    
     boardDetail();
 
+    
+
   }, []);
-    console.log(params.bno)
+
 
 
 
@@ -63,18 +52,15 @@ const BoardDetail = () => {
       relative='path'
     >Back to board</Link>
     <br/>
+
       <div className={classes.text_bno}>{board?.bno}</div>
       <div className={classes.title} >{board?.title}</div>
       <div className={classes.writer} >{board?.writer}</div>
       <div className={classes.write_date} >{board?.writeDate}</div>
       <div className={classes.content}> {board?.content}</div>
 
-        
-
       <BoardComment bno={params.bno} />
-      <BoardComment bno={params.bno} />
-      <BoardComment bno={params.bno} />
-      <BoardComment bno={params.bno} />
+  
       
 
 
