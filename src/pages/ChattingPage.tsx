9fb926eac0fe751ec3e72056c1070da226dbb23b
seohan1010 <SEOhan12@>
2024-11-30 21:03:29 +0900

    Chatting, ChattingBlock test okay condition

--- src/pages/ChattingPage.tsx
+++ src/pages/ChattingPage.tsx
@@ -1,12 +1,15 @@
 import classes from '../style/pages_style/chattingPage.module.css';
 import Chatting from '../components/Chatting';
-
+import Footer from '../components/Footer';
 const ChattingPage = () => {
 
 
     return (
         <div className={classes.mainpage__wrap}>
+                <div style={{width:'100vw',height: '50px',backgroundColor:'purple'}}></div>
                 <Chatting></Chatting>
+                <div style={{width:'100vw',height:'50vh'}}></div>
+                <Footer></Footer>
         </div>
     )
 }
